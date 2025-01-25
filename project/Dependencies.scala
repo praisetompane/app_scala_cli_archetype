e67892ff2ee3c575f8faec4adcff1c09f101f4de
Praise Tompane <praisetompane@icloud.com>
2025-01-25 10:26:16 +0200

    fix: add dev containers

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val scalaVersion = "2.13.1"
+  val scalaVersion = "2.13.16"
 
   lazy val scalaTestFunSuite =
     "org.scalatest" %% "scalatest-funsuite" % "3.2.10"
